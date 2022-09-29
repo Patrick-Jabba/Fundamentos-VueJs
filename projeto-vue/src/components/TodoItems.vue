358d14368a680225ea801cb2b9850e90aee10f71
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-09-29 17:00:05 -0300

    finalização do projeto

---
+++ projeto-vue/src/components/TodoItems.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <TodoItem
+    v-for="todo in $store.state.todos"
+    :key="todo.id"
+    :todo="todo"
+     />
+   
+  </div>
+</template>
+
+<script>
+import TodoItem from "@/components/TodoItem";
+
+export default {
+  components: {
+    TodoItem,
+  },
+};
+</script>
+
